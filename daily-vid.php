21669ddcff4767e5f9084f0eeece7b7956cf92d9
DimitriNakos <dimi1nakos@gmail.com>
2022-01-12 00:51:02 -0500

    content swap.  More to figure out tomorrow.

--- daily-vid.php
+++ daily-vid.php
@@ -4,16 +4,16 @@
  * Description: Insert a Video Underneath the first H2 of every post relating to that day.
  */
 
-// <iframe width="420" height="315"
-// src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
-// </iframe>
 
 // make an object of the dates and their associated video.
 
+// depending on the day of the week change the video
+// $dayOfWeek = the_weekday();
 
-$weekday = array();
 
+// function that replaces content with video
 function addVideo( $the_content ) {
+
         if ( is_single() ) {
             return '<iframe width="560" height="315" src="https://www.youtube.com/embed/du-TY1GUFGk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
